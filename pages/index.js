227ac8d3360e880521eb5b67bf29098a2433664c
arega <arega@4over.com>
2019-01-19 19:48:24 -0800

    next js running

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
+import io from 'socket.io';
+
+var socket = io('http://localhost/');
+socket.on('connect', function () {
+  socket.send('hi');
+
+  socket.on('message', function (msg) {
+    // my msg
+  });
+});
+
 export default () => <div>Welcome to next.js!</div>
