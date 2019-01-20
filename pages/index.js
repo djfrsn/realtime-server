828189324d9664b81d21600e0d737437536f265a
Dennis Jefferson <dennismjefferson@gmail.com>
2019-01-19 20:48:25 -0800

    use local host

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,74 @@
-import { Component } from 'react'
-import io from 'socket.io-client'
-import fetch from 'isomorphic-fetch'
+import { Component } from 'react';
+import io from 'socket.io-client';
+import fetch from 'isomorphic-fetch';
 
 class HomePage extends Component {
   // fetch old messages data from the server
   static async getInitialProps({ req }) {
-    const response = await fetch('https://next-socket-io.now.sh/messages')
-    const messages = await response.json()
-    return { messages }
+    const response = await fetch('http://localhost:3000/messages');
+    const messages = await response.json();
+    return { messages };
   }
 
   static defaultProps = {
-    messages: [],
-  }
+    messages: []
+  };
 
   // init state with the prefetched messages
   state = {
     field: '',
-    messages: this.props.messages,
-  }
+    messages: this.props.messages
+  };
 
   // connect to WS server and listen event
   componentDidMount() {
-    this.socket = io('https://next-socket-io.now.sh/')
-    this.socket.on('message', this.handleMessage)
+    this.socket = io('http://localhost:3000/');
+    this.socket.on('message', this.handleMessage);
   }
 
   // close socket connection
   componentWillUnmount() {
-    this.socket.off('message', this.handleMessage)
-    this.socket.close()
+    this.socket.off('message', this.handleMessage);
+    this.socket.close();
   }
 
   // add messages from server to the state
-  handleMessage = (message) => {
-    this.setState(state => ({ messages: state.messages.concat(message) }))
-  }
+  handleMessage = message => {
+    this.setState(state => ({ messages: state.messages.concat(message) }));
+  };
 
   handleChange = event => {
     this.setState({ field: event.target.value });
-  }
+  };
 
   // send messages to server and add them to the state
   handleSubmit = event => {
-    event.preventDefault()
+    event.preventDefault();
 
     // create message object
     const message = {
-      id: (new Date()).getTime(),
-      value: this.state.field,
-    }
+      id: new Date().getTime(),
+      value: this.state.field
+    };
 
     // send object to WS server
-    this.socket.emit('message', message)
+    this.socket.emit('message', message);
 
     // add it to state and clean current input value
     this.setState(state => ({
       field: '',
       messages: state.messages.concat(message)
-    }))
-  }
+    }));
+  };
 
   render() {
     return (
       <main>
         <div>
           <ul>
-            {this.state.messages.map(message =>
+            {this.state.messages.map(message => (
               <li key={message.id}>{message.value}</li>
-            )}
+            ))}
           </ul>
           <form onSubmit={this.handleSubmit}>
             <input
@@ -81,8 +81,8 @@ class HomePage extends Component {
           </form>
         </div>
       </main>
-    )
+    );
   }
 }
 
-export default HomePage
+export default HomePage;
